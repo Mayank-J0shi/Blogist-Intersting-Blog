bec78f2ce2eb36a33fda1e9c4f463bfc94a41aac
Mayank Joshi <mayank2000j@gmail.com>
2022-03-07 01:30:39 +0530

    Completed Version 1

--- client/src/context/Actions.js
+++ client/src/context/Actions.js
@@ -11,6 +11,19 @@ export const LoginFailure = () => ({
   type: "LOGIN_FAILURE",
 });
 
-export const Logout = ()=>({
-  "type" : "LOGOUT",
-})
+export const Logout = () => ({
+  type: "LOGOUT",
+});
+
+export const UpdateStart = (userCredentials) => ({
+  type: "UPDATE_START",
+});
+
+export const UpdateSuccess = (user) => ({
+  type: "UPDATE_SUCCESS",
+  payload: user,
+});
+
+export const UpdateFailure = () => ({
+  type: "UPDATE_FAILURE",
+});
